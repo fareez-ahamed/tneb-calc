af2c4c42a756ea38a652c9d1a38af49124137b59
Fareez Ahamed <fareez.ahamed@gmail.com>
2019-03-30 19:59:03 +0530

    Added deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,12 +1,13 @@
-ng build
+ng build --base-href "/tneb-calc/"
 if($LASTEXITCODE -eq 0)
 {
     $originalLocation = Get-Location
-    Set-Location -Path .\dist\tneb-calc
+    Set-Location -Path .\dist\tneb-app
     git init
     git add .
     git commit -m "Bismillah"
     git remote add origin "git@github.com:fareez-ahamed/tneb-calc.git"
+    git branch gh-pages
     git push origin gh-pages --force
     Set-Location $originalLocation
 }
